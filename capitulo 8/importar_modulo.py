8528700a34ef76496e2f1bb33637faa5713be739
judy <judysantos169@gmsil.com>
2023-12-13 21:56:57 -0300

    new updates

--- capitulo 8/importar_modulo.py
+++ capitulo 8/importar_modulo.py
@@ -1,4 +1,10 @@
-import statistics
 
-numeros = [1, 34, 6, 81, 39 , 4, 2]
-print(statistics.median(numeros))
+# pandas é uma biblioteca, usada para manipulação e análises de dados.
+import pandas as pd
+quarto_valor ={"quarto 1" : 50,
+               "quarto 2" : 35,
+               "quarto 3" : 67}
+passando_valor= pd.Series(quarto_valor)
+print(passando_valor)
+
+# para saber mais acesse a documentação https://pandas.pydata.org/docs/
