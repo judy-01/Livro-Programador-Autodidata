625f54a2dcdd037425340ef866c3c0c4e9844f75
judy <judysantos169@gmsil.com>
2023-12-13 21:25:36 -0300

    new updates

---
+++ capitulo 8/cubo.py
@@ -0,0 +1,11 @@
+# função numero_ao_cubo, recebe o paramêtro numero
+def numero_ao_cubo(numero):
+    # retona esse numero ao cubo
+    return  numero * 3
+
+
+
+
+    
+
+    
---
+++ capitulo 8/importarCubo.py
@@ -0,0 +1,7 @@
+## importa a função numero_ao_cubo, do modulo cubo
+from cubo import numero_ao_cubo
+# define 5 como valor 
+numero = 5
+#chama a função com o numero acima
+#reultado = numero_ao_cubo(numero) s
+print(f'{numero} ao cubo é {numero_ao_cubo(numero)}')
